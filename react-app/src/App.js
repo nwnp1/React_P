bcf5c8271e8d42d75767079d6db721fa50f4e59d
nwnp1 <stotoyaa@naver.com>
2022-01-09 17:53:17 +0900

    React_state_mode_rendering
    state의 mode 값에 따라 컴포넌트의 렌더링 결과가 달라지게 조건문 추가

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode:'welcome',
+      mode:'read',
       subject:{title:'WEB', sub:"World Wide Web!"},
       welcome:{title:'Welcome', desc:'Hello, React!'},
       contents:[
@@ -20,11 +20,20 @@ class App extends Component {
     }
   }
   render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
