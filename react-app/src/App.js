a49ff482432da640a424b1201888b75d2ca94d97
nwnp1 <stotoyaa@naver.com>
2022-01-12 01:41:00 +0900

    react_concat
    리액트를 이용해 Create 수행.
    리로드 과정 없이 텍스트 입력 후 화면에 링크 및 내용 뜨게 하기

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -46,11 +46,14 @@ class App extends Component {
     } else if(this.state.mode === 'create') {
       _article = <CreateContent onSubmit={function(_title, _desc) {
         this.max_content_id = this.max_content_id+1;
-        this.state.contents.push(
+        var _contents = this.state.contents.concat(
           {id:this.max_content_id, title:_title, desc:_desc}
-        );
+        )
+        /* this.state.contents.push(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        ); */
         this.setState({
-          contents:this.state.contents
+          contents:_contents
         });
         console.log(_title, _desc);
       }.bind(this)}></CreateContent>
