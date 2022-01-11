c6340fe172c625ee6686fed701242a525eb2ed0a
nwnp1 <stotoyaa@naver.com>
2022-01-12 02:28:50 +0900

    raect_concat_update
    concat과 shouldComponentUpdate이용하여 화면에 출력

--- react-app/src/components/TOC.js
+++ react-app/src/components/TOC.js
@@ -1,31 +1,42 @@
 import React, { Component } from 'react';
 
 class TOC extends Component {
-    render() {
-      console.log('TOC render');
+  shouldComponentUpdate(newProps, newState) {
+    console.log("==>TOC render shouldComponentUpdate",
+      newProps.data,
+      this.props.data
+    );
+    if(this.props.data === newProps.data) { 
+      return false;
+    }
+    return true;
+  } 
+  render() {
+    console.log("==>TOC render");
     var lists = [];
     var data = this.props.data;
     var i = 0;
     while(i < data.length) {
-        lists.push(<li key={data[i].id}>
+      lists.push(
+        <li key={data[i].id}>
           <a 
-            href={"/content/" + data[i].id}
+            href={"/content/"+data[i].id}
             data-id={data[i].id}
-            onClick={function (e) {
+            onClick={function (id, e) {
               e.preventDefault();
-              this.props.onChangePage(e.target.dataset.id);
-            }.bind(this)}
+              this.props.onChangePage(id);
+            }.bind(this, data[i].id)}
           >{data[i].title}</a></li>);
-        i = i + 1;
+      i = i + 1;
     }
-      return (
-        <nav>
-            <ul>
-                {lists}
-            </ul>
-        </nav>
-      );
-    }
+    return (
+      <nav>
+          <ul>
+            {lists}
+          </ul>
+      </nav>
+    );
   }
+}
 
-  export default TOC;
+export default TOC; 
