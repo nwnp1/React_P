af264b813315aa926e18469e6908a65e052ed0a4
nwnp1 <stotoyaa@naver.com>
2022-01-14 01:34:44 +0900

    react_function_delete_component
    함수 예제 최종

--- react-func-class-style-coding/src/App.js
+++ react-func-class-style-coding/src/App.js
@@ -2,11 +2,25 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  var [funcShow, setFuncShow] =useState(true);
+  var [classShow, setClassShow] =useState(true);
+  
   return (
     <div className="container">
       <h1>Hello World</h1>
-      <FuncComp initNumber={2}></FuncComp>
-      <ClassComp initNumber={2}></ClassComp>
+      <input type="button" value="remove func" onClick={
+        function() {
+          setFuncShow(false)
+        }
+      }></input>
+      <input type="button" value="remove class" onClick={
+        function() {
+          setClassShow(false)
+        }
+      }></input> 
+      {funcShow ? <FuncComp initNumber={2}></FuncComp> : null}     
+      {classShow ? <ClassComp initNumber={2}></ClassComp> : null}      
+
     </div> 
   );
 }
@@ -23,6 +37,14 @@ function FuncComp(props) {
   // var setDate = dateState[1];
 
   var [_date, setDate] = useState((new Date()).toString());  
+
+  useEffect(function() {
+    console.log('%cfunc => useEffect (componentDidMount) '+(++funcId), funcStyle);
+    document.title = number;
+    return function() {
+      console.log('%cfunc => useEffect return (componentWillUnMount) '+(++funcId), funcStyle);
+    }
+  }, []);
 
   //side effect
   useEffect(function() {
