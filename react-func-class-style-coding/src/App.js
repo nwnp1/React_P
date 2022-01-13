6540ffc9e4b1f5ef3bdd3622681d08894ee60467
nwnp1 <stotoyaa@naver.com>
2022-01-13 16:51:37 +0900

    react_state_class
    class 컴포넌트에서 state사용법

---
+++ react-func-class-style-coding/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+      <FuncComp initNumber={2}></FuncComp>
+      <ClassComp initNumber={2}></ClassComp>
+    </div>
+  );
+}
+
+function FuncComp(props) { //컴포넌트 함수를 리액트가 호출할 때 첫 번째 파라미터에 인자 값으로 props 값을 전달하도록 약속
+  return (
+    <div className="container">
+      <h2>function style component</h2>
+      <p>Number : {props.initNumber}</p>
+      </div> 
+  );
+}
+
+class ClassComp extends React.Component{
+  state = {
+    number:this.props.initNumber
+  }
+  render() {
+    return (
+      <div className="container">
+        <h2>class style component</h2>
+        <p>Number : {this.state.number}</p>
+        <input type="button" value="random" onClick={
+          function(){
+            this.setState({number:Math.random()});
+          }.bind(this)
+        }></input>
+      </div>
+    )
+  }
+}
+
+export default App;
