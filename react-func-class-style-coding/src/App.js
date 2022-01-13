184dd998122f40d042040dc1e09490a2b0feb190
nwnp1 <stotoyaa@naver.com>
2022-01-14 00:36:32 +0900

    react_function,state,clean up
    react 함수에서 라이프사이클 구현-state, clean up

--- react-func-class-style-coding/src/App.js
+++ react-func-class-style-coding/src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-import React from 'react';
 
 function App() {
   return (
@@ -8,35 +7,93 @@ function App() {
       <h1>Hello World</h1>
       <FuncComp initNumber={2}></FuncComp>
       <ClassComp initNumber={2}></ClassComp>
+    </div> 
+  );
+}
+
+var funcStyle = 'color:blue';
+var funcId = 0;
+function FuncComp(props) {
+  var numberState = useState(props.initNumber);
+  var number = numberState[0];
+  var setNumber = numberState[1];
+  
+  // var dateState = useState((new Date()).toString());
+  // var _date = dateState[0];
+  // var setDate = dateState[1];
+
+  var [_date, setDate] = useState((new Date()).toString());  
+
+  //side effect
+  useEffect(function() {
+    console.log('%cfunc => useEffect (componentDidMount & componentDidUpdate) '+(++funcId), funcStyle);
+    document.title = number + ' : ' + _date;
+    return function () {
+      console.log('%cfunc => useEffect return (componentDidMount & componentDidUpdate) '+(++funcId), funcStyle);
+    }
+  });
+
+  console.log('%cfunc => render '+(++funcId), funcStyle);
+  return (
+    <div className="container">
+      <h2>function style component</h2>
+      <p>Number : {number}</p>
+      <p>Date : {_date}</p>
+      <input type="button" value="random" onClick={
+        function() {
+          setNumber(Math.random());
+        }
+      }></input>
+      <input type="button" value="date" onClick={
+        function() {
+          setDate((new Date()).toString());
+        }
+      }></input>
     </div>
   );
 }
 
-function FuncComp(props) { //컴포넌트 함수를 리액트가 호출할 때 첫 번째 파라미터에 인자 값으로 props 값을 전달하도록 약속
-  return (
-    <div className="container">
-      <h2>function style component</h2>
-      <p>Number : {props.initNumber}</p>
-      </div> 
-  );
-}
-
-class ClassComp extends React.Component{
+var classStyle = 'color:red';
+class ClassComp extends React.Component {
   state = {
-    number:this.props.initNumber
+    number:this.props.initNumber,
+    date:(new Date()).toString()
+  }
+  componentWillMount() {
+    console.log('%cclass => componentWillMount', classStyle);
+  }
+  componentDidMount() {
+    console.log('%cclass => componentDidMount', classStyle);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('%cclass => shouldComponentUpdate', classStyle);
+    return true;
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log('%cclass => componentWillUpdate', classStyle);
+  }
+  componentDidUpdate(nextProps, nextState) {
+    console.log('%cclass => componentDidUpdate', classStyle);
   }
   render() {
+    console.log('%cclass => render', classStyle);
     return (
       <div className="container">
         <h2>class style component</h2>
         <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
         <input type="button" value="random" onClick={
-          function(){
+          function() {
             this.setState({number:Math.random()});
           }.bind(this)
         }></input>
+        <input type="button" value="date" onClick={
+          function() {
+            this.setState({date:(new Date()).toString()});
+          }.bind(this)
+        }></input>
       </div>
-    )
+    );
   }
 }
 
