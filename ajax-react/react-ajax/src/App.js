ad0a277f93bac1a8716ffbed5b7b5f313febfc73
nwnp1 <stotoyaa@naver.com>
2022-01-20 01:28:46 +0900

    react_Ajax_loading
    로딩 예제

--- ajax-react/react-ajax/src/App.js
+++ ajax-react/react-ajax/src/App.js
@@ -18,7 +18,9 @@ class Nav extends Component {
     }
     return (
       <nav>
-        {listTag}
+        <ul>
+          {listTag}
+        </ul>
       </nav>
     );
   }
@@ -36,40 +38,76 @@ class Article extends Component {
   }
 }
 
+class NowLoading extends Component {
+  render(){
+    return <div>Now Loading...</div>
+  }
+}
+
 class App extends Component {
   state = { //state 추가
-    article:{title:'Welcome', desc:'Hello, React & Ajax'},
-    list:[]
+    article:{
+      item:{title:'Welcome', desc:'Hello, React & Ajax'},
+      isLoading:false
+    },
+    list:{
+      items:[],
+      isLoading:false
   }
+}
   componentDidMount() {
+    var newList = Object.assign({}, this.state.list, {isLoading:true});
+    this.setState({list:newList});
     fetch('list.json') //fetch API는 첫번째 인자로 가져오려고 하는 데이터의 주소 받음
       .then(function(result) { //list.json에 담긴 데이터를 브라우저가 가져오면 가져온 데이터를 어떻게 처리할 것인지 구현
         return result.json(); //result를 대상으로 json()메서드 호출하면 브라우저가 list.json의 파일의 데이터를 자바스크립트 객체로 변환
       })
       .then(function(json) { //변환된 자바스크립트 객체는 두 번째 then 함수의 인자로 공급된 함수의 첫 번째 인자로 전달
         console.log(json);
-        this.setState({list:json});
+        this.setState({list:{
+          items:json,
+          isLoading:false
+        }});
       }.bind(this));
   }
   render() {
+    var NavTag = null;
+    if(this.state.list.isLoading) {
+      NavTag=<NowLoading></NowLoading>
+    } else {
+      NavTag = <Nav list={this.state.list.items} onClick={function(id){ //Nav 컴포넌트 내에서 id를 인자로 props에 전달받은 onClick 함수 호출
+        var newArticle = Object.assign({}, this.state.article, {isLoading:true});
+        this.setState({article:newArticle});
+        fetch(id+'.json') //각 id에 해당하는 JSON 파일을 읽기 위해 fetch API 사용
+          .then(function(result) {
+          return result.json();
+        })
+        .then(function(json) {
+          this.setState({ //데이터 App 컴포넌트의 state에 저장
+            article:{
+              item:{
+                title:json.title,
+                desc:json.desc
+            },
+            isLoading:false
+          }
+        })
+      }.bind(this));
+    }.bind(this)}></Nav>
+  }
+  
+  var ArticleTag = null;
+  if(this.state.article.isLoading) {
+    ArticleTag = <NowLoading></NowLoading>
+  } else {
+    ArticleTag = <Article title = {this.state.article.item.title} desc={this.state.article.item.desc}></Article>
+  }
+
     return (
       <div className="App">
         <h1>WEB</h1>
-        <Nav list={this.state.list} onClick={function(id){ //Nav 컴포넌트 내에서 id를 인자로 props에 전달받은 onClick 함수 호출
-          fetch(id+'.json') //각 id에 해당하는 JSON 파일을 읽기 위해 fetch API 사용
-            .then(function(result) {
-              return result.json();
-            })
-            .then(function(json) {
-              this.setState({ //데이터 App 컴포넌트의 state에 저장
-                article:{
-                  title:json.title,
-                  desc:json.desc
-                }
-              });
-            }.bind(this));
-        }.bind(this)}></Nav>
-        <Article title={this.state.article.title} desc={this.state.article.desc}></Article>
+        {NavTag}
+        {ArticleTag}
       </div>
     );
   }
