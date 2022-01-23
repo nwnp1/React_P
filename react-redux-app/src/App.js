3a82cb337c659c653050689b9d32ebe9fde0d1e9
nwnp1 <stotoyaa@naver.com>
2022-01-23 17:34:17 +0900

    reaact-redux_Components
    컴포넌트 깔끔하게 정리

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,28 +1,7 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import './App.css';
-
-class AddNumber extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Add Number</h1>
-        <input type="button" value="+"></input>
-        <input type="text" value="0"></input>
-      </div>
-    );
-  }
-}
-
-class AddNumberRoot extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Add Number Root</h1>
-        <AddNumber></AddNumber>
-      </div>
-    );
-  }
-}
+import AddNumberRoot from './components/AddNumberRoot';
+import DisplayNumberRoot from './components/DisplayNumberRoot';
 
 function App() {
   return (
@@ -32,28 +11,6 @@ function App() {
       <DisplayNumberRoot></DisplayNumberRoot>
     </div>
   );
-}
-
-class DisplayNumber extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Display Number</h1>
-        <input type="text" value="0" readOnly></input>
-      </div>
-    )
-  }
-}
-
-class DisplayNumberRoot extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Display Number Root</h1>
-        <DisplayNumber></DisplayNumber>
-        </div>
-    )
-  }
 }
 
 export default App;
