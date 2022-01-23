8352e108dbd94e006d56c508918802c89685b341
nwnp1 <stotoyaa@naver.com>
2022-01-24 01:24:52 +0900

    react_non_redux
    redux 없이 컴포넌트 상태 연결

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import AddNumberRoot from './components/AddNumberRoot';
 import DisplayNumberRoot from './components/DisplayNumberRoot';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Root</h1>
-      <AddNumberRoot></AddNumberRoot>
-      <DisplayNumberRoot></DisplayNumberRoot>
-    </div>
-  );
+class App extends Component {
+  state = {number:0}
+  render () {
+    return (
+      <div className="App">
+        <h1>Root</h1>
+        <AddNumberRoot onClick={function(size) { //onClick props에 size값을 인자로 하는 함수 전달
+          this.setState({number:this.state.number + size}); //전달된 size값을 state.number에 더함
+        }.bind(this)}></AddNumberRoot>
+        <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-redux-app/src/components/AddNumberRoot.jsx
+++ react-redux-app/src/components/AddNumberRoot.jsx
@@ -6,7 +6,9 @@ export default class AddNumberRoot extends Component {
       return (
         <div>
           <h1>Add Number Root</h1>
-          <AddNumber></AddNumber>
+          <AddNumber onClick={function(size) { //AddNumber의 +버튼 클릭했을 때 AddNumber의 state.size값을 인자로 호출할 것
+            this.props.onClick(size);
+          }.bind(this)}></AddNumber>
         </div>
       );
     }
